38b465847a4e57e6fcfcc2d253bf31bc3c1a3cac
Nazli Ayaz <hnazliayaz@gmail.com>
2021-06-08 12:52:19 +0100

    remove app start as not needed

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -32,11 +32,11 @@ app.get('/api', (req, res) => {
   res.send(quotes);
 });
 
-const port = process.env.port || 3333;
-const server = app.listen(port, () => {
-  console.log('Listening at http://localhost:' + port + '/api');
-});
-server.on('error', console.error);
+// const port = process.env.port || 3333;
+// const server = app.listen(port, () => {
+//   console.log('Listening at http://localhost:' + port + '/api');
+// });
+// server.on('error', console.error);
 
 module.exports = app;
 module.exports.handler = serverless(app);
